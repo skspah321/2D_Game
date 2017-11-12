5f5393495412c3a3a55e7eab412f56707894c2bf
이현 <skspah888@naver.com>
2017-11-13 00:51:28 +0900

    11/12

---
+++ Map/school_chip_02.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="school_chip_02" tilewidth="32" tileheight="32" tilecount="400" columns="8">
+ <image source="학교맵칩/ST-Schl-WI02.png" width="256" height="1600"/>
+</tileset>
